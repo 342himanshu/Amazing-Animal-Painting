9d8913971c298a58d26f8bc2de1720b8b8d0e45c
342himanshu <342himanshu@gmail.com>
2024-07-17 02:03:12 +0530

    first commit

--- AMAZING-ANIMAL-PAINTINGS/src/app/app.module.ts
+++ AMAZING-ANIMAL-PAINTINGS/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { ProductModule } from './product/product.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ProductModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    ProductModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- AMAZING-ANIMAL-PAINTINGS/src/app/product/product.service.ts
+++ AMAZING-ANIMAL-PAINTINGS/src/app/product/product.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +10,9 @@ import { environment } from 'src/environments/environment';
 export class ProductService {
   private apiUrl = environment.apiUrl + '/products';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
 }
