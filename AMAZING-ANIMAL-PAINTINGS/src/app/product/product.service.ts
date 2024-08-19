20e4d248864e56e702679016b85a3e5214159e07
342himanshu <342himanshu@gmail.com>
2024-08-20 00:11:07 +0530

    env

--- AMAZING-ANIMAL-PAINTINGS/src/app/product/product.service.ts
+++ AMAZING-ANIMAL-PAINTINGS/src/app/product/product.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private apiUrl = environment.apiUrl + '/products';
 
-  constructor() { }
+  constructor() {}
 }
